be24d80900b391fbfe4589033387db8fd0c4b773
Caio Tracera <ctrcra@gmail.com>
2020-04-14 11:22:04 -0300

    added JWT token validation

--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -12,7 +13,7 @@ class AuthenticateUserService {
 	public async execute({
 		email,
 		password,
-	}: Request): Promise<{ user: User }> {
+	}: Request): Promise<{ user: User; token: string }> {
 		const usersRepository = getRepository(User);
 
 		const user = await usersRepository.findOne({ where: { email } });
@@ -25,7 +26,12 @@ class AuthenticateUserService {
 			throw new Error('Incorrect e-mail/password combination');
 		}
 
-		return { user };
+		const token = sign({}, '8d4bde4ec6aece4c8eb5a94e73ca1556', {
+			subject: user.id,
+			expiresIn: '1d',
+		});
+
+		return { user, token };
 	}
 }
 
