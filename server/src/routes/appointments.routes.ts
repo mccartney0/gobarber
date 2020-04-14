50131758c8dc736bb51f21d8a60313de9b595c1e
Caio Tracera <ctrcra@gmail.com>
2020-04-14 08:26:09 -0300

    added TypeORM

---
+++ server/src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (req, res) => {
+	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+	const appointments = await appointmentsRepository.find();
+
+	return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+	try {
+		const { provider, date } = req.body;
+
+		const parsedDate = parseISO(date);
+
+		const createAppointment = new CreateAppointmentService();
+
+		const appointment = await createAppointment.execute({
+			date: parsedDate,
+			provider,
+		});
+
+		return res.json(appointment);
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+});
+
+export default appointmentsRouter;
