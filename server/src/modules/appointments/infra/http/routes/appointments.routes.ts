21fb7ddae0f8de095e89b43ce815488c5914aed6
Caio Tracera <ctrcra@gmail.com>
2020-05-16 14:33:20 -0300

    Provider dashboard: list all the appointments of the day

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appontmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 appointmentsRouter.post('/', appontmentsController.create);
 
 export default appointmentsRouter;
