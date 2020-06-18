94f1de292c6be7e1161f0287c9a71259f130dd47
Caio Tracera <ctrcra@gmail.com>
2020-06-18 19:18:45 -0300

    feat: created new pages and routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -5,6 +5,9 @@ import {
 } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
@@ -17,6 +20,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
